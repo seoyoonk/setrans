e6ec0872fa58869c5902fd9bacc59cee412793f5
mimela98 <mimela98@gmail.com>
2018-01-03 17:35:09 +0900

    a

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 import { FinishPage } from '../finish/finish';
 import { LoadPage } from '../load/load';
 import { LoadCancelPage } from '../load_cancel/load_cancel';
+import { ModalController } from 'ionic-angular';
 import { ReadyPage } from '../ready/ready';
+import { DetailPage } from '../detail/detail';
 
 import { RestProvider } from '../../providers/rest';
+import { FCM } from '@ionic-native/fcm';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -17,12 +20,19 @@ export class TabsPage {
   tab3Root = FinishPage;
   tab4Root = LoadCancelPage;
 
-  constructor(public rest: RestProvider) {
+  notiCnt: number = 0;
 
+  constructor(public rest: RestProvider, private fcm: FCM, public modalCtrl: ModalController) {
+
+    fcm.onNotification().subscribe(data => {
+      const profileModal = this.modalCtrl.create(DetailPage, data);
+      profileModal.present();
+      
+    });
   }
 
   ionViewDidLoad() {
- 
+
     this.rest.getReadyList();
   }
 }
